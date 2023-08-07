5962454bb0925a5b38d42734e56538642cd25e3e
Jon Baker <jonbaker85@gmail.com>
2023-08-07 20:42:40 +1000

    fix up cloudflare-ddns

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -74,8 +74,8 @@ data "http" "ipv4" {
   url = "http://ipv4.icanhazip.com"
 }
 
-resource "cloudflare_record" "ipv4" {
-  name    = "ipv4"
+resource "cloudflare_record" "ingress" {
+  name    = "ingress"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
   value   = chomp(data.http.ipv4.response_body)
   proxied = true
@@ -86,7 +86,7 @@ resource "cloudflare_record" "ipv4" {
 resource "cloudflare_record" "root" {
   name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  value   = "ingress.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
   proxied = true
   type    = "CNAME"
   ttl     = 1
